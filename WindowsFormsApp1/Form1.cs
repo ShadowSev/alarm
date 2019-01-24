995dd352d064a1e78ed3690a49d10a6ea8f9453f
S4sh <sev-shadow@ya.ru>
2019-01-24 22:55:02 +0300

    Start2

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -28,12 +28,38 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             string strCmdText1;
-            //string strCmdText2;
+            string strCmdText2;
             InputSimulator Simulator = new InputSimulator();
             strCmdText1 = "Schtasks /Change /TN alarm_1.1 /ST 06:11:00";
-            //strCmdText2 = "Schtasks /Change /TN alarm_1.2 /ST 06:10:00";
-            Process.Start("powershell.exe", strCmdText1);
-            //Process.Start("powershell.exe", strCmdText2);
+            strCmdText2 = "Schtasks /Change /TN alarm_1.2 /ST 06:11:00";
+            Process p = new Process();
+            p.StartInfo.FileName = "powershell.exe";
+            p.StartInfo.Arguments = strCmdText1;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = false;
+            p.StartInfo.Verb = "runas";
+            p.Start();
+            Simulator.Mouse.MoveMouseTo(30000, 35000);
+            Simulator.Mouse.Sleep(20);
+            Simulator.Mouse.LeftButtonClick();
+            Simulator.Mouse.Sleep(20);
+            Simulator.Keyboard.TextEntry("193203");
+            Simulator.Mouse.Sleep(20);
+            Simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            Simulator.Mouse.Sleep(20);
+            p.StartInfo.FileName = "powershell.exe";
+            p.StartInfo.Arguments = strCmdText2;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = false;
+            p.StartInfo.Verb = "runas";
+            p.Start();
+            Simulator.Mouse.Sleep(20);
+            Simulator.Mouse.LeftButtonClick();
+            Simulator.Mouse.Sleep(20);
+            Simulator.Keyboard.TextEntry("193203");
+            Simulator.Mouse.Sleep(20);
+            Simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            Simulator.Mouse.Sleep(20);
         }
     }
 }
